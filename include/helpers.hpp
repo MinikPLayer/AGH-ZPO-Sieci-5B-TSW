396375c019ffd211eb67ad9a5ef6280c28cfc545
Minik <minexplaypl@gmail.com>
2021-12-19 16:15:17 +0100

    Dodany opis grupy w nagłówkach plików

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -1,1 +1,2 @@
+// 5B: Tomecki (408146), Sztefko (407388), Walawski (406822)
 #pragma once
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// 5B: Tomecki (408146), Sztefko (407388), Walawski (406822)
 #include <iostream>
 #include "test.hpp"
 
